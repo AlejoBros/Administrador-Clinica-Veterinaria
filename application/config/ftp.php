e7919717429233d59a81c3255d97110b5a13df00
AlejoBros <alejandro.martinez.az@gmail.com>
2019-07-13 19:38:44 -0500

    Update ftp.php

--- application/config/ftp.php
+++ application/config/ftp.php
@@ -1,9 +1,9 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['hostname'] = 'ftp.dogmedicat.com';
-$config['username'] = 'u224527687.consultas';
-$config['password'] = 'consultas';
+$config['hostname'] = 'fantasy';
+$config['username'] = 'fantasy';
+$config['password'] = 'fantasy';
 $config['port'] = 21;
 $config['passive'] = FALSE;
 $config['debug'] = FALSE;
